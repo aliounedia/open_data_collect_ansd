8281e90eec53daba2de2ba042a5a7763fab20993
dia <adia@pcci.sn>
2013-04-10 18:41:05 +0200

    update

---
+++ utils/datasets.py
@@ -0,0 +1,60 @@
+# -*- coding: latin-1 -*-
+import ckanclient, sys, codecs,re
+data_link_desc = "datas/descriptions"
+
+class CkanPost(ckanclient.CkanClient):
+    def __init__(self, base_location=None, api_key=None, is_verbose=False,
+                 http_user=None, http_pass=None, **kwargs):
+        ckanclient.CkanClient.__init__(
+           self, 
+           base_location = base_location,
+           api_key=api_key,
+           is_verbose = is_verbose,
+           http_user = http_user,
+           http_pass  = http_pass)
+
+    def ansd_dataset(self):
+        """ return the set of datasets for ansd """
+        i = 0
+        list =[]
+        for package in  self.package_list():
+            if re.match("__www_ansd", package):
+                list.append(package)
+        return list
+    
+    def __repr__(self):
+        """ return the set of datasets for ckan """
+        i = 0
+        for package in  self.package_list():
+                print package
+                i +=1
+        print 'len :', i
+
+ckan_post  = CkanPost("http://datahub.io/api",
+        "b3c845db-f5f8-44af-a493-5ca5f6eccd94",
+        True,
+        "xxx",
+        "xxx")
+
+ansd_dataset     =     ckan_post.ansd_dataset
+if __name__ == '__main__':
+    # http://thedatahub.org/api/rest/package
+    # http://datahub.io/group/country-sn
+    ckan_post  = CkanPost("http://datahub.io/api",
+        "b3c845db-f5f8-44af-a493-5ca5f6eccd94",
+        True,
+        "xxx",
+        "xxx")
+    print 'ckan isntance', ckan_post
+    #ckan_post.test_post_package_resource()
+    ckan_post.__repr__()
+           
+           
+
+
+         
+
+
+
+          
+          
